053184d3e685fd2403af4ef960a739900643f700
wilson <wilsonkhoo93@gmail.com>
2020-06-08 17:17:47 +0800

    Add empty page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   mount_devise_token_auth_for 'User', at: 'users'
+  root 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
